ab10209f59e9b8b0ca27b8d468fb7579cad5811d
minh <minhchiu.official@gmail.com>
2025-05-03 23:37:48 +0700

    feat: init source

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+    </main>
+  </SidebarProvider>
+</template>
